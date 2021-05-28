ff834164d7c77783dbfba523c752c588a407f1e3
dhaifur <dhaifurr@gmail.com>
2021-05-28 22:01:17 +0700

    Uji Coba 3

--- index.php
+++ index.php
@@ -4,11 +4,9 @@
     $conn = $db->connect();
     $output = array();
     $query="SELECT * FROM detail_wayang";
-    $result = mysqli_query($conn, $query);
-    if(mysqli_num_rows($result) > 0){
-        while($obj = mysqli_fetch_object($result)){
-            $output[] = $obj;
-        }
+    $result = $conn->prepare($query);
+    while($obj = mysqli_fetch_object($result)){
+        $output[] = $obj;
     }
     header('Content-Type: JSON');
     echo json_encode($output, JSON_PRETTY_PRINT);
