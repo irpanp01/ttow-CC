6f4e15f68603ceff05c0e1e0c09ba6d7eafefa76
dhaifur <dhaifurr@gmail.com>
2021-05-29 08:16:56 +0700

    Uji Coba 7

--- index.php
+++ index.php
@@ -3,15 +3,13 @@
     $db = new auth();
     $conn = $db->connect();
 
+    $output = array();
     $query="SELECT * FROM detail_wayang";
     $statement = $conn->prepare($query);
     $statement->execute();
-    $output = array();
-    
-    while($obj = $statement->fetch_array(MYSQLI_ASSOC)) {
+    while($obj = $statement->fetch(PDO::FETCH_ASSOC) {
         $output[] = $obj;
     }
-
-    mysqli_close($statement);
+    header('Content-type: JSON');
     echo json_encode($output, JSON_PRETTY_PRINT);
 ?>
