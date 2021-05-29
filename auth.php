3393db10b421c4366d6c9cd6a986c83ab2567496
dhaifur <dhaifurr@gmail.com>
2021-05-29 09:21:49 +0700

    Uji Coba 8

--- auth.php
+++ auth.php
@@ -1,25 +1,32 @@
 <?php 
-	class auth {
-		private $host;
-		private $dbName;
-		private $user;
-		private $pass;
+	// class auth {
+	// 	private $host;
+	// 	private $dbName;
+	// 	private $user;
+	// 	private $pass;
         
-        public function __construct(){
-            $this->host = getenv('MYSQL_HOST');
-            $this->dbName = getenv('MYSQL_DB');
-            $this->user = getenv('MYSQL_USER');
-            $this->pass = getenv('MYSQL_PASS');
-        }
+    //     public function __construct(){
+    //         $this->host = getenv('MYSQL_HOST');
+    //         $this->dbName = getenv('MYSQL_DB');
+    //         $this->user = getenv('MYSQL_USER');
+    //         $this->pass = getenv('MYSQL_PASS');
+    //     }
 
-		public function connect() {
-			try {
-				$conn = new PDO($this->host . '; dbname=' . $this->dbName, $this->user, $this->pass);
-				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				return $conn;
-			} catch( PDOException $e) {
-				echo 'Database Error: ' . $e->getMessage();
-			}
-		}
-	}
+	// 	public function connect() {
+	// 		try {
+	// 			$conn = new PDO($this->host . '; dbname=' . $this->dbName, $this->user, $this->pass);
+	// 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// 			return $conn;
+	// 		} catch( PDOException $e) {
+	// 			echo 'Database Error: ' . $e->getMessage();
+	// 		}
+	// 	}
+	// }
+	
+	$host = "/cloudsql/wayang-314813:asia-southeast2:ttow";
+    $user = "root";
+    $pass = "wyg2021";
+    $db = "wayangkulit";
+
+    $mysqli = mysqli_connect($host,$user,$pass,$db) or die ("Tidak dapat terkoneksi dengan database");
 ?>
--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <?php
-    include 'auth.php';
-    $db = new auth();
-    $conn = $db->connect();
+    // include 'auth.php';
+    // $db = new auth();
+    // $conn = $db->connect();
 
+    // $output = array();
+    // $query="SELECT * FROM detail_wayang";
+    // $statement = $conn->prepare($query);
+    // $statement->execute();
+    // while($obj = $statement->fetch(PDO::FETCH_ASSOC) {
+    //     $output[] = $obj;
+    // }
+    // header('Content-type: JSON');
+    // echo json_encode($output, JSON_PRETTY_PRINT);
+    include 'auth.php'
     $output = array();
-    $query="SELECT * FROM detail_wayang";
-    $statement = $conn->prepare($query);
-    $statement->execute();
-    while($obj = $statement->fetch(PDO::FETCH_ASSOC) {
-        $output[] = $obj;
+
+    $sql = "SELECT * FROM detail_wayang;"
+    $query = mysqli_query($mysqli,$sql) or die ("Tidak Terhubung");
+    
+    while($obj = mysqli_fetch_array($query)){
+        $output[]=$obj;
     }
-    header('Content-type: JSON');
-    echo json_encode($output, JSON_PRETTY_PRINT);
+    header('Conten Type : JSON');
+    echo json_encode($output,JSON_PRETTY_PRINT);
 ?>
