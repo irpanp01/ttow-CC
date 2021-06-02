187e9162cccac7d03fb775786dc37be5fae23932
dhaifur <dhaifurr@gmail.com>
2021-06-03 00:24:00 +0700

    Uji Coba 38

--- getwayang.php
+++ getwayang.php
@@ -1,16 +1,16 @@
 <?php
     include 'auth.php';
-    $getid = $_GET['id'];
+    $getnm = $_GET['nm'];
 
     // $query="DELETE FROM detail_wayang WHERE id='$id'";
     // $result = mysqli_query($conn, $query);
     $db = new auth();
     $conn = $db->connect();
-    $query="SELECT * FROM detail_wayang WHERE id_wayang=:id";
+    $query="SELECT * FROM detail_wayang WHERE nm_wayang=:nm";
     $statement = $conn->prepare($query);
     //$statement->bindParam(":getid",$getid);
     $output = array();
-    if($statement->execute(['id' => $getid])){
+    if($statement->execute(['nm' => $getnm])){
         while($row = $statement->fetchAll(PDO::FETCH_ASSOC)) {
             $output['result'] = $row;
         }
